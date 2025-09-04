fd5273ad7540e855781424b97feeb10518295380
Andrés López <andres.lopez@gomeruapps.com>
2025-09-04 16:02:13 +0200

    Actualización de librerías y código para targetSdk 36. Versión actualizada.

--- app/src/main/java/com/nc248radio/ui/main/MainActivity.kt
+++ app/src/main/java/com/nc248radio/ui/main/MainActivity.kt
@@ -2,6 +2,12 @@ package com.nc248radio.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.updatePadding
 import com.nc248radio.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,7 +16,27 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.enableEdgeToEdge(window)
+
         setContentView(R.layout.activity_main)
+
+        val root = findViewById<View>(android.R.id.content)
+
+        //Color de los iconos de StatusBar y NavigationBAr
+        WindowInsetsControllerCompat(window, root).apply {
+            isAppearanceLightStatusBars = false
+            isAppearanceLightNavigationBars = false
+        }
+
+        //Padding top y bottom
+        ViewCompat.setOnApplyWindowInsetsListener(root) { view, insets ->
+            val sysBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(top = sysBars.top, bottom = sysBars.bottom)
+
+            insets
+        }
+
     }
 
 }
--- app/src/main/java/com/nc248radio/ui/player/InfoFragment.kt
+++ app/src/main/java/com/nc248radio/ui/player/InfoFragment.kt
@@ -1,12 +1,17 @@
 package com.nc248radio.ui.player
 
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.color.MaterialColors
 import com.nc248radio.databinding.LayoutInfoBinding
 
 class InfoFragment: BottomSheetDialogFragment() {
@@ -16,6 +21,34 @@ class InfoFragment: BottomSheetDialogFragment() {
     }
 
     private lateinit var dataBinding: LayoutInfoBinding
+
+    override fun onStart() {
+        super.onStart()
+        val w = dialog?.window ?: return
+
+        WindowCompat.enableEdgeToEdge(w)
+
+        //Color de los iconos de StatusBar y NavigationBAr
+        WindowInsetsControllerCompat(w, requireView()).apply {
+            isAppearanceLightStatusBars = false
+            isAppearanceLightNavigationBars = false
+        }
+
+        //Color de fondo del NavigationBar
+        val sheet = dialog?.findViewById<View>(
+            com.google.android.material.R.id.design_bottom_sheet
+        ) ?: return
+
+        val colorSecondary = MaterialColors.getColor(
+            sheet,
+            com.google.android.material.R.attr.colorSecondary
+        )
+
+        ViewCompat.setBackgroundTintList(
+            sheet,
+            ColorStateList.valueOf(colorSecondary)
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
